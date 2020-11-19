d90364c0938c2c51377860ff498e45dd29a3b718
Bojana Cvetkovska <bojana.cvetkovska98@gmail.com>
2020-11-19 19:04:31 +0100

    Lecture2

---
+++ Lectures/Lecture2/data-structures/strings.js
@@ -0,0 +1,134 @@
+const hello = 'Hello World';
+const hi = 'Hey there';
+
+const stringWithNumbers = '1234546364';
+
+// String lenght - kolku e dolzinata na stringot
+const helloLenght = hello.length;
+// ''
+// ""
+// `` template string
+console.log('The length of the hello is ', helloLenght);
+console.log('The length of' +hello+ 'is', helloLenght);
+
+const msg = 'The length of' +hello+ 'is' + helloLenght;
+console.log(msg);
+
+//template string
+const templateString = `The length of ${hello} is $(helloLenght)`;
+console.log(templateString);
+
+console.log(''.length); // the empty string has a length of 0
+console.log(' '.length); //space=> 1 
+
+const firstString = 'Hi';
+const secondString = 'Hello';
+console.log(firstString + secondString);
+
+const string = 'hello world, I am student at Semos Education';
+console.log(string);
+
+
+console.log(string.toLocaleLowerCase()) //site mali bukvi
+console.log(string.toLocaleUpperCase()) //site golemi bukvi
+
+const concatFirstAndSecondString = firstString.concat(secondString);
+console.log(concatFirstAndSecondString);
+
+console.log(secondString.concat(firstString));
+
+console.log("2020 ".concat('Hello').concat(' ').concat('World'))
+console.log(''.concat(20, 20));
+console.log('' + 20 + 20); // same as line 41
+
+console.log(5 + '55');
+
+
+// startsWith 
+const stringText= 'Hello November December';
+const stringWith = 'Hello';
+const checkIfStringStartsWith = stringText.startsWith (stringWith);
+console.log ('Starts with',checkIfStringStartsWith); 
+
+// not case sensetive
+// JavaString !== javascript 
+console.log('JavaScript'.startsWith('JavaS'));
+
+//substring string.substring (startIndex,endIndex)
+const stringText1 = 'Hello December';
+// index 
+// 0....N-1
+// 0 = H
+// 1 = e
+// 2 = l
+// 3 = l
+// 4 = 0
+// 5 = ' '
+// 6 = D
+// 7 = e
+
+const subStringText1 = stringText1.substring (0);
+console.log(subStringText1);
+const subStringTextBetween2and4 = stringText1.substring (2, 4);
+console.log(subStringTextBetween2and4);
+const subStringText3 = stringText1.substring (2, 6);
+console.log(subStringText3);
+const subStringText4 = stringText1.substring(4, stringText1.lenght, -1);
+console.log(subStringText4);
+
+//trim = remove whitespaces
+const stringTextForTrim = '                  Hello        World                 ';
+console.log(stringTextForTrim);
+
+const trimmed = stringTextForTrim.trim();
+console.log(trimmed);
+
+//trimStart()
+//trimEnd()
+
+
+// split
+const stringSplit = "hello world, I am a student at Semos Education";
+console.log(stringSplit.split());
+const wordsWithSplit = stringSplit.split(' ');
+console.log(wordsWithSplit);
+
+const stringSplit1 = 'hello,world,I,am student,at,Semos';
+console.log(stringSplit1);
+console.log(stringSplit1.split(','));
+
+const text = "hello world!, I am a student at Semos Education. I am Software Developer. I have more than six years exp"
+const countSent = text.split('.');
+console.log(countSent);
+console.log('How?', countSent.length);
+
+//slice
+//str.slice(startIndex,endIndex)
+const stringSlice = "hello world, I am a student at Semos Education";
+const slice = stringSlice.slice(9);
+console.log(slice);
+
+const sliceBetween = stringSlice.slice(15,23);
+console.log(sliceBetween);
+
+const lastThreeChars = stringSlice.slice(-3);
+console.log(lastThreeChars);
+
+console.log(stringSlice.slice(-22, -2));
+
+//includes 
+//case sensitive
+const textForIncludes = "hello world, I am a student at Semos Education";
+const word = 'world';
+const isFound = textForIncludes.includes(word);
+console.log(isFound);
+
+//indexOf
+const findWord = 'Hello World'.indexOf('Hello');
+console.log(findWord); // 0 (found)
+
+const notFound = 'Hello World'.indexOf('hello');
+console.log(notFound); // -1 (not found)
+
+console.log('Hello World'.indexOf('World', 1)); // 6
+console.log('Hello World'.indexOf('World', 7)); // -1 the value is not found
---
+++ Lectures/Lecture2/operators.js
@@ -0,0 +1,73 @@
+//Logical operators 
+//Se koristat kako Boolean vrednost, ili e true ili false.
+
+let a = true;
+let b = false;
+
+//Logical AND (&&)
+
+let logicalAnd = a && b; //false
+//true && true = true
+//true && false = false
+//false && true = false
+//false && false = false
+
+let password=false;
+let confirmPassword=true;
+
+let checkPass=password && confirmPassword;
+console.log('checkPass', checkPass);
+
+
+const number1=4;
+const number2=2;
+const arebothNumbersGtThree=number1 > 3 && number2 > 3; // ako number2 e < 3 togash kje e true
+
+console.log('Check number with logical AND', arebothNumbersGtThree )
+
+//Logigal OR (||)
+// False e SAMO ako se dvete false, site ostanite se true
+const logicalOr=number1 > 3 || number2 > 3;
+console.log('Check number with logical OR', logicalOr)
+
+console.log('Check OR', 3 > 2 || -2 > 0 || '2'=== 2 || 'hello'=='hello'); // (||) potrebno e samo edno true za uslovot da e true
+console.log('Check AND', 3 > 2 && -2 > 0 && '2'=== 2 && 'hello'=='hello'); // (&&) mora site da se true za da uslovot e true
+
+const checkANDandOR = (3 >2 && -2 < 0) || ('2'== 2 && 'hello'==='hello'); //true || true => true
+console.log(checkANDandOR); //true
+const checkLogical = (3 >2 || -2 < 0) && ('2'=== 2 || 'hello'!=='hello');// true && false => false
+console.log(checkLogical); //false
+
+//Logical NOT (!)
+//Negation
+// truth to falsity <=> i obratno
+const checkWithNegation = !(number1 > 3 || number2 < -3);
+const makeNegation = !checkWithNegation;
+console.log('Negation', checkWithNegation);
+console.log('Negation 222', makeNegation);
+
+const checkWithNegation1 = (number1 > 3 || number2 < -3);
+const makeNegation1 = !checkWithNegation1;
+console.log(checkWithNegation1);
+console.log(makeNegation1);
+
+const checkWithNegation2 = (number1 > 3 && !(number2 < -3));
+const makeNegation2 = !checkWithNegation2;
+console.log(checkWithNegation2);
+console.log(makeNegation2);
+
+const check1 = (3 > 2);
+console.log(!!!check1); // so eden ! e false, so 2 !! e true, so 3 !!! e false i se redi taka..
+
+// Ternary operator result ? true : false
+const ternaryCheck = number1 > 3 ? true : false;
+console.log('Ternary', ternaryCheck);
+
+const ternary1 = number1 < 3 ? true : false;
+console.log('Ternary1', ternary1);
+
+const shortSyntaxTernary1 = !!(4 < 3); //  same as line 66
+console.log('Short Syntax Ternary', shortSyntaxTernary1);
+
+const ternaryString = 4 !== 4 ? 'Correct' : 'Wrong'; // vo prviot del se slucuva true, vo vtoriot del e false
+console.log('Return String With', ternaryString);
